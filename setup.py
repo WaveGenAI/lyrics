d7afb85ba6e3c59f996437a24862272a699246f3
Jourdelune <jourdelune863@gmail.com>
2024-05-08 21:38:12 +0200

    [update] pass from pyproject.tom to setuptools

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="lyrics_whisper",
+    version="0.0.1",
+    description="A small package to convert lyric with whisper",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/WaveGenAI/lyrics.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+    packages=find_packages(),
+    install_requires=requirements,
+)
